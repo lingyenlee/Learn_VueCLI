558bd1e7b4ac1790cccdfb8b2978a56dbf1d4eea
lingyenlee <lingyenlee@gmail.com>
2020-02-25 11:59:41 +1100

    upate

---
+++ slot/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <quote>
+          <h2 slot="title">{{quoteTitle}}</h2>
+          <p slot="content">A wonderful quote</p>
+        </quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./component/Quote.vue";
+
+export default {
+  data() {
+    return {
+      quoteTitle: "The Quote"
+    };
+  },
+  components: {
+    quote: Quote
+  }
+};
+</script>
+
+<style>
+h2 {
+  color: red;
+}
+</style>
